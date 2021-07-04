a3a0a1e0e581d0b70ced629aeeeb592bfc253ba0
liuhh666233 <liuxiaobo666233@gmail.com>
2021-07-04 21:23:48 +0800

    Add lxb.nix

--- users/lxb.nix
+++ users/lxb.nix
@@ -2,7 +2,7 @@
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users = {
-     users.liuxb = {
+     users.lxb = {
      isNormalUser = true;
      initialHashedPassword = lib.mkDefault "$6$c7xWgHnMCJKXlPHK$2Owbgw3Y5z/pgwD76O45nPzuRtABjN.Mr6M.yO1jOteDyVsXSOqgzgkAl1sD1kIowEQRFIoyXT45j8ZLmZ5SF1";
      uid = 1001;
