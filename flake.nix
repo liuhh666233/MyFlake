b236418d213a66c503af3b111cd9392024e5451d
liuhh666233 <liuxiaobo666233@gmail.com>
2021-07-05 09:50:31 +0800

    Add devShell.

--- flake.nix
+++ flake.nix
@@ -7,15 +7,36 @@
     };
 
   outputs = { self, nixpkgs, ... }@inputs: 
+  let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+  in 
     {
       nixosConfigurations = {
         
         lxb = nixpkgs.lib.nixosSystem rec{
-          system = "x86_64-linux";
+          inherit system;
           modules = [
             ./machines/lxb
           ];
         };
       };
+
+      devShell."${system}" = pkgs.mkShell rec {
+        name = "nix-demo-env";
+        
+        buildInputs = with pkgs; [
+          (python38.withPackages(ps: with ps; [ jupyter jupyter_core notebook ipython ipykernel systemd]))
+        ];
+
+        shellHook = ''
+          export PS1="$(echo -e '\uf3e2') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+        '';
+      };
+
+
     };
 }
