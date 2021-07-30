a494a44cfe2398865276df14676040f54239984c
liuhh666233 <liuxiaobo666233@gmail.com>
2021-07-30 08:47:58 +0800

    fish plugins

--- users/lxb.nix
+++ users/lxb.nix
@@ -2,15 +2,18 @@
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users = {
-     users.lxb = {
-     isNormalUser = true;
-     initialHashedPassword = lib.mkDefault "$6$c7xWgHnMCJKXlPHK$2Owbgw3Y5z/pgwD76O45nPzuRtABjN.Mr6M.yO1jOteDyVsXSOqgzgkAl1sD1kIowEQRFIoyXT45j8ZLmZ5SF1";
-     uid = 1001;
-     home = "/home/lxb";
-     description = "The user lxb.";
-     programs.fish.enable = true;
-     shell = pkgs.fish;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+        users.lxb = {
+        isNormalUser = true;
+        initialHashedPassword = lib.mkDefault "$6$c7xWgHnMCJKXlPHK$2Owbgw3Y5z/pgwD76O45nPzuRtABjN.Mr6M.yO1jOteDyVsXSOqgzgkAl1sD1kIowEQRFIoyXT45j8ZLmZ5SF1";
+        uid = 1001;
+        home = "/home/lxb";
+        description = "The user lxb.";
+        extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+        programs.fish.enable = true;
+        shell = pkgs.fish;
+        programs.fish.plugins = [{
+            name = "z";  
+            }];
    };
  };
 }
