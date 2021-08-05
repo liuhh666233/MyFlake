b3f5127b43c65d67b26496b40cd0acec6fb43567
liuhh666233 <liuxiaobo666233@gmail.com>
2021-08-05 11:09:42 +0800

    Add borgbackup server

--- machines/lxb/import_nix.nix
+++ machines/lxb/import_nix.nix
@@ -3,14 +3,15 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../../modules/i18n.nix
-      ../../modules/binaryCaches.nix
-      ../../modules/systemPackages.nix
-      ../../modules/enable_flake.nix
-      ../../modules/network.nix
-    #   ../../modules/service/clickhouse
-      ../../users/lxb.nix
-      ../../users/liuxb.nix
+        ./hardware-configuration.nix
+        ../../modules/i18n.nix
+        ../../modules/binaryCaches.nix
+        ../../modules/systemPackages.nix
+        ../../modules/enable_flake.nix
+        ../../modules/network.nix
+        #   ../../modules/service/clickhouse
+        ../../users/lxb.nix
+        ../../users/liuxb.nix
     ];
+
 }
--- users/lxb.nix
+++ users/lxb.nix
@@ -3,13 +3,13 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
     users = {
             users.lxb = {
-            isNormalUser = true;
-            initialHashedPassword = lib.mkDefault "$6$c7xWgHnMCJKXlPHK$2Owbgw3Y5z/pgwD76O45nPzuRtABjN.Mr6M.yO1jOteDyVsXSOqgzgkAl1sD1kIowEQRFIoyXT45j8ZLmZ5SF1";
-            uid = 1001;
-            home = "/home/lxb";
-            description = "The user lxb.";
-            extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-            shell = pkgs.fish;
+                isNormalUser = true;
+                initialHashedPassword = lib.mkDefault "$6$c7xWgHnMCJKXlPHK$2Owbgw3Y5z/pgwD76O45nPzuRtABjN.Mr6M.yO1jOteDyVsXSOqgzgkAl1sD1kIowEQRFIoyXT45j8ZLmZ5SF1";
+                uid = 1001;
+                home = "/home/lxb";
+                description = "The user lxb.";
+                extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+                shell = pkgs.fish;
         };
     };
     programs.fish.enable = true;
@@ -30,4 +30,29 @@
     programs.fish.shellInit = ''
       source (${pkgs.z-lua}/bin/z --init fish | psub)
     '';
+
+    services.borgbackup.repos = {
+        backup_repos = {
+            user = "lxb";
+            authorizedKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB//oIgJern4hluCMaQ3ZwCBb27yhjsWg6ZtAW/3lnDO root@nixos"
+            ] ;
+            path = "/var/lib/backup" ;
+        };
+    };
+
+    services.borgbackup.jobs = {
+            backupToLocalServer = {
+                paths = [ "/home/lxb/github" ];
+                doInit = true;
+                repo =  "lxb@nixos:." ;
+                encryption = {
+                    mode = "repokey-blake2";
+                    passCommand = "cat /run/keys/borgbackup_passphrase";
+                };
+                environment = { BORG_RSH = "ssh -i /run/keys/id_ed25519_backup"; };
+                compression = "auto,lzma";
+                startAt = "daily";
+            };
+        };
 }
