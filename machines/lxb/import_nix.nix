adaa35debe52be1140dde2d5a0be71fd5f5a979d
liuhh666233 <liuxiaobo666233@gmail.com>
2021-08-05 23:23:01 +0800

    Add demo-vm

--- machines/lxb/import_nix.nix
+++ machines/lxb/import_nix.nix
@@ -14,4 +14,27 @@
         ../../users/liuxb.nix
     ];
 
+    services.borgbackup.repos = {
+        backup_repos = {
+            authorizedKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdOZ8cHGtI5ZCQCnNdFFAWL5lYhPCQk0fvU8gyv6/Uh root@nixos"
+            ] ;
+            path = "/var/lib/backup" ;
+        };
+    };
+
+    services.borgbackup.jobs = {
+        backupToLocalServer = {
+            paths = [ "/home/lxb/github" ];
+            doInit = true;
+            repo =  "borg@nixos:." ;
+            encryption = {
+                mode = "repokey-blake2";
+                passCommand = "cat /run/keys/passwd";
+            };
+            environment = { BORG_RSH = "ssh -i /run/keys/id_ed25519_backup"; };
+            compression = "auto,lzma";
+            startAt = "daily";
+        };
+    };
 }
