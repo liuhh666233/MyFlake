336273ea59c8704ed719b196ed9c3dda4e33ec73
liuhh666233 <liuxiaobo666233@gmail.com>
2021-08-09 17:16:07 +0800

    Fixed

--- modules/network.nix
+++ modules/network.nix
@@ -2,7 +2,8 @@
 
 {
         networking.useDHCP = false;
-        networking.interfaces.enp0s13f0u2u2.useDHCP = true;
+        networking.hostName = "lxb";
+        networking.interfaces.enp0s13f0u2u4.useDHCP = true;
         networking.interfaces.wlp165s0.useDHCP = true;
 
         nixpkgs.config.allowUnfree = true;
