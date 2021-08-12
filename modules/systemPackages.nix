40cfdaf5955f10e697b494b702bf7df12e407662
liuhh666233 <liuxiaobo666233@gmail.com>
2021-08-12 15:18:49 +0800

    Add pyyaml

--- modules/systemPackages.nix
+++ modules/systemPackages.nix
@@ -1,85 +1,100 @@
-{config, pkgs, ...}:
-{ 
- # List packages installed in system profile. To search, run:
+{ config, pkgs, ... }: {
+  # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # Dedit
-      vim
-      neovim
-      typora
-      emacs
-      zeal
+    vim
+    neovim
+    typora
+    emacs
+    zeal
 
     # System
-      wget
-      spotify
-      ibus
-      fcitx
-      docker
-      lsd
-      glances
-      duf
-      p7zip
-      unrar
-      inetutils
-      dpkg
-      ag
-      tig
-      tmux
-      starship
-      fish
-      z-lua
-      bat
-      tree
-      ncdu
-      ripgrep
-      synergy
-      remmina
-      borgbackup
-
+    wget
+    spotify
+    ibus
+    fcitx
+    docker
+    lsd
+    glances
+    duf
+    p7zip
+    unrar
+    inetutils
+    dpkg
+    ag
+    tig
+    tmux
+    starship
+    fish
+    z-lua
+    bat
+    tree
+    ncdu
+    ripgrep
+    synergy
+    remmina
+    borgbackup
 
     # Work
-      vscode
-      google-chrome
-      git
-      freeoffice
-      goffice
-      awscli2
-      gftp
-      zoom-us
-      dbeaver
-      clickhouse-cli
-      graphviz
-      mailspring
-      nixfmt
+    vscode
+    google-chrome
+    git
+    freeoffice
+    goffice
+    awscli2
+    gftp
+    zoom-us
+    dbeaver
+    clickhouse-cli
+    graphviz
+    mailspring
+    nixfmt
 
     # VPN
-      v2ray
-      qv2ray
+    v2ray
+    qv2ray
 
     # Python
-      python3
-      (python38.withPackages(ps: with ps; [ jupyter jupyter_core notebook ipython ipykernel pandas numpy systemd click jinja2 clickhouse-driver flask autopep8 pip]))
+    python3
+    (python38.withPackages (ps:
+      with ps; [
+        jupyter
+        jupyter_core
+        notebook
+        ipython
+        ipykernel
+        pandas
+        numpy
+        systemd
+        click
+        jinja2
+        clickhouse-driver
+        flask
+        autopep8
+        pip
+        pyyaml
+      ]))
 
     # C++
-      gcc
-      gdb
-      clang
-      nodejs
-      lua
-      cmake
-      doxygen
-      doxygen_gui
-     
+    gcc
+    gdb
+    clang
+    nodejs
+    lua
+    cmake
+    doxygen
+    doxygen_gui
+
     # Gnome extension
-      chrome-gnome-shell
-      gnome.gnome-tweak-tool
-      gnome.gnome-remote-desktop
-      gnomeExtensions.appindicator
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.system-monitor
-      gnome.gnome-weather
-      gnomeExtensions.sound-output-device-chooser
-      
+    chrome-gnome-shell
+    gnome.gnome-tweak-tool
+    gnome.gnome-remote-desktop
+    gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.system-monitor
+    gnome.gnome-weather
+    gnomeExtensions.sound-output-device-chooser
+
   ];
 }
