2f5a21d0e163b36235dbce836b8fe1b0f2a56f3a
liuhh666233 <liuxiaobo666233@gmail.com>
2021-08-15 17:57:59 +0800

    Add nginx services.

--- modules/service/nginx/default.nix
+++ modules/service/nginx/default.nix
@@ -1,13 +1,25 @@
 { config, pkgs, ... }: {
   config = {
-    services.nginx.enable = true;
-    services.nginx.virtualHosts."myhost.org" = {
-      addSSL = true;
-      enableACME = true;
-      root = "/var/lib/wonder/warehouse/report";
-    };
-    systemd.services.nginx.serviceConfig.ReadWritePaths =
-      [ "/var/spool/nginx/logs/" ];
 
+    services.nginx = {
+      enable = true;
+      # Enable recommended proxy settings.
+      recommendedProxySettings = true;
+      # Enable recommended TLS settings.
+      recommendedTlsSettings = true;
+      virtualHosts."localhost" = {
+        listen = [{
+          addr = "0.0.0.0";
+          port = 10000;
+        }];
+        locations = {
+          "/" = { proxyPass = "http://localhost:5000"; };
+          "~ \"/monitor/(.*)\"" = { proxyPass = "http://localhost:5000/$1"; };
+          "~ \"/monitor2/(.*)\"" = { proxyPass = "http://localhost:5001/$1"; };
+        };
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ 80 443 10000 ];
   };
 }
