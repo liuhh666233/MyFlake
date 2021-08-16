40a6c9b44cb4d10bbb6317a4c5b7d392003f54fb
liuhh666233 <liuxiaobo666233@gmail.com>
2021-08-16 16:54:58 +0800

    delete nginx server.

--- modules/service/nginx/default.nix
+++ modules/service/nginx/default.nix
@@ -12,8 +12,16 @@
       http {
           server{
               listen 10000;
+              server_name monitor.wondercapital.xyz;
               location / {
                 proxy_pass http://192.168.110.161:10000;
+            }
+          }
+          server{
+              listen 10000;
+              server_name clickhouse.wondercapital.xyz;
+              location / {
+                proxy_pass http://192.168.110.161:9001;
             }
           }
       }
@@ -22,8 +30,7 @@
                 server 192.168.110.161:22;
                 }
             server { 
-                    listen 10000;
-                    server_name 127.0.0.1;
+                    listen 9000;
                     proxy_pass ssh;
                     proxy_connect_timeout 1h;
                     proxy_timeout 1h;
@@ -36,6 +43,6 @@
 
     };
 
-    networking.firewall.allowedTCPPorts = [ 80 443 10000 ];
+    networking.firewall.allowedTCPPorts = [ 10000 ];
   };
 }
