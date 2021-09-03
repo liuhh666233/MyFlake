71960a3425d2db83e5e613a49c1288c63cf67a5e
liuhh666233 <liuxiaobo666233@gmail.com>
2021-09-03 16:39:39 +0800

    Add github runner.

--- modules/service/github-runner/default.nix
+++ modules/service/github-runner/default.nix
@@ -3,9 +3,9 @@
 
     services.github-runner = {
       enable = true;
-      url = "https://github.com/quant-wonderland/integration-test";
+      url = "https://github.com/quant-wonderland";
       tokenFile = "/home/lxb/github_action/actions-runner/nixos.token";
-      name = "lxb";
+      name = "nixos-test";
     };
 
   };
