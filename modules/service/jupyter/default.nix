8f1d82dee23caa54c79635b0b9971b32b81ae18e
liuhh666233 <liuxiaobo666233@gmail.com>
2021-09-21 14:50:39 +0800

    fixed bug.

--- modules/service/jupyter/default.nix
+++ modules/service/jupyter/default.nix
@@ -7,20 +7,15 @@
     user = "lxb";
     group = "users";
     password =
-      "argon2:$argon2id$v=19$m=10240,t=10,p=8$OEgjjTojGX8e+bmi431VBg$WjUsHTZdM2EOIjpwgn5dXw";
-    notebookConfig = ''
-      c.NotebookApp.allow_remote_access = True
-          '';
+      "'argon2:$argon2id$v=19$m=10240,t=10,p=8$OEgjjTojGX8e+bmi431VBg$WjUsHTZdM2EOIjpwgn5dXw'";
+    notebookConfig = "c.NotebookApp.allow_remote_access = True";
     kernels = {
       python3 = let
         env = (pkgs.python3.withPackages (pythonPackages:
           with pythonPackages; [
-            jupyter
-            jupyter_core
-            notebook
-            ipython
             ipykernel
             pandas
+            scikitlearn
             numpy
             systemd
             click
@@ -35,9 +30,9 @@
             pytz
           ]));
       in {
-        displayName = "Python 3 for machine learning";
+        displayName = "Python 3";
         argv = [
-          "''${env.interpreter}"
+          "${env.interpreter}"
           "-m"
           "ipykernel_launcher"
           "-f"
