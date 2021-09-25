f7e7bd64d4dcdb54479af47b8b89348e82111b89
liuhh666233 <liuxiaobo666233@gmail.com>
2021-09-25 10:08:25 +0800

    Adjust nix binary caches

--- modules/binaryCaches.nix
+++ modules/binaryCaches.nix
@@ -1,13 +1,11 @@
 { config, pkgs, ... }: {
   nix = {
     binaryCaches = [
-      "http://192.168.110.15:5000/"
       "https://cache.nixos.org/"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     ];
     trustedBinaryCaches = [
-      "http://192.168.110.15:5000/"
       "https://cache.nixos.org/"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store/"
