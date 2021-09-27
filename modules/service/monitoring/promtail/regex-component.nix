dae621d6b0adde20deaffad94a8eb9300b0739e0
liuhh666233 <liuxiaobo666233@gmail.com>
2021-09-27 13:09:33 +0800

    Adjust excaping to \\\\.

--- modules/service/monitoring/promtail/regex-component.nix
+++ modules/service/monitoring/promtail/regex-component.nix
@@ -6,25 +6,25 @@ rec {
   # 匹配服务日志 
   logRegexSet = {
     jsy-archiver =
-      "^\\\\\\\\s*${timeRegexSet.Y-M-D_H-M-S}\\\\\\\\s*${levelRegesSet.lowerLogLevel}\\\\\\\\s*${any-chars}(${jobNameRegexSet.jsy-archiver})?${any-chars}(${conclusionRegexSet.datapipeline})?${any-chars}$";
+      "^${escaping}s*${timeRegexSet.Y-M-D_H-M-S}${escaping}s*${levelRegesSet.lowerLogLevel}${escaping}s*${any-chars}(${jobNameRegexSet.jsy-archiver})?${any-chars}(${conclusionRegexSet.datapipeline})?${any-chars}$";
     jsy-importer =
-      "^\\\\\\\\s*${timeRegexSet.Y-M-D_H-M-S}\\\\\\\\s*${levelRegesSet.lowerLogLevel}\\\\\\\\s*${any-chars}(${jobNameRegexSet.jsy-importer})?${any-chars}(${conclusionRegexSet.datapipeline})?${any-chars}$";
+      "^${escaping}s*${timeRegexSet.Y-M-D_H-M-S}${escaping}s*${levelRegesSet.lowerLogLevel}${escaping}s*${any-chars}(${jobNameRegexSet.jsy-importer})?${any-chars}(${conclusionRegexSet.datapipeline})?${any-chars}$";
     wind-importer =
-      "^\\\\\\\\s*${timeRegexSet.Y-M-D_H-M-S}\\\\\\\\s*${levelRegesSet.lowerLogLevel}\\\\\\\\s*${any-chars}(${jobNameRegexSet.wind-importer})?${any-chars}(${conclusionRegexSet.datapipeline})?${any-chars}$";
+      "^${escaping}s*${timeRegexSet.Y-M-D_H-M-S}${escaping}s*${levelRegesSet.lowerLogLevel}${escaping}s*${any-chars}(${jobNameRegexSet.wind-importer})?${any-chars}(${conclusionRegexSet.datapipeline})?${any-chars}$";
   };
 
   # 匹配日志时间 
   timeRegexSet = {
     # 格式： [ 2021-09-26 08:49:31.227 ]
     Y-M-D_H-M-S =
-      "\\\\\\\\[\\\\\\\\s?(?P<time>\\\\\\\\d{4}-\\\\\\\\d{2}-\\\\\\\\d{2}\\\\\\\\s?\\\\\\\\d{2}:\\\\\\\\d{2}:\\\\\\\\d{2}\\\\\\\\.\\\\\\\\d{3})\\\\\\\\s?\\\\\\\\]";
+      "${escaping}[${escaping}s?(?P<time>${escaping}d{4}-${escaping}d{2}-${escaping}d{2}${escaping}s?${escaping}d{2}:${escaping}d{2}:${escaping}d{2}${escaping}.${escaping}d{3})${escaping}s?${escaping}]";
   };
 
   # 匹配日志级别 
   levelRegesSet = {
     # 格式： [info] [warning] 
     lowerLogLevel =
-      "\\\\\\\\[(?P<level>(info|warning|error|critical|trace|debug))\\\\\\\\]";
+      "${escaping}[(?P<level>(info|warning|error|critical|trace|debug))${escaping}]";
     # 格式： INFO WARNING ERROR
     upperLogLevel = "(?P<level>(INFO|WARNING|ERROR|CRITICAL|TRACE|DEBUG))";
   };
@@ -33,23 +33,24 @@ rec {
   jobNameRegexSet = {
     #  格式：'JSY::Archive::PriceDBDaily 2021-09-24'
     jsy-archiver =
-      "(\\\\\\\\'JSY::Archive::(?P<archiver_job>\\\\\\\\S+\\\\\\\\s?\\\\\\\\d{4}-\\\\\\\\d{2}-\\\\\\\\d{2})\\\\\\\\'";
+      "(${escaping}'JSY::Archive::(?P<archiver_job>${escaping}S+${escaping}s?${escaping}d{4}-${escaping}d{2}-${escaping}d{2})${escaping}'";
     #  格式：Job 'JsyMinBarArcImporterDaily 2021-09-24'
     jsy-importer =
-      "(job|Job)\\\\\\\\s?\\\\\\\\'(?P<importer_job>\\\\\\\\S+\\\\\\\\s?\\\\\\\\d{4}-\\\\\\\\d{2}-\\\\\\\\d{2})\\\\\\\\'";
+      "(job|Job)${escaping}s?${escaping}'(?P<importer_job>${escaping}S+${escaping}s?${escaping}d{4}-${escaping}d{2}-${escaping}d{2})${escaping}'";
     #  格式： Job 'WindIndexWeightDeriverJob 2021-09-26' | Job 'WindDailyImporterJob /var/lib/wonder/warehouse/raw/wind/__DATA__/AShareCapitalization 2021-09-26'
     wind-importer =
-      "(job|Job)\\\\\\\\s?\\\\\\\\'(?P<importer_job>\\\\\\\\S+\\\\\\\\s?(\\\\\\\\S+)?\\\\\\\\s?\\\\\\\\d{4}-\\\\\\\\d{2}-\\\\\\\\d{2})\\\\\\\\'";
+      "(job|Job)${escaping}s?${escaping}'(?P<importer_job>${escaping}S+${escaping}s?(${escaping}S+)?${escaping}s?${escaping}d{4}-${escaping}d{2}-${escaping}d{2})${escaping}'";
   };
 
   # 匹配任务运行结果 
   conclusionRegexSet = {
     # 格式：conclusion = OK | conclusion = ALREADY_EXISTS: Output file '/var/lib/wonder/warehouse/archive/price_db/2021/09/24.arc' exists
-    datapipeline = "conclusion =\\\\\\\\s?(?P<conclusion>(OK|\\\\\\\\S+:))s?";
+    datapipeline =
+      "conclusion =${escaping}s?(?P<conclusion>(OK|${escaping}S+:))s?";
   };
 
   #  匹配任意多个字符
   any-chars = ".*";
-  #  匹配任意多个空格
-  any-blanks = "\\\\\\\\s*";
+  # YAML中需要使用 \\ 进行转义，\\替换到YAML中会被合并为\，因此需要使用  \\\\\\\\
+  escaping = "\\\\\\\\";
 }
