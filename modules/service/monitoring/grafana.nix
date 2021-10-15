79e0501cccf7a728447aa2b234d66aba3b4fca56
liuhh666233 <liuxiaobo666233@gmail.com>
2021-10-15 17:34:31 +0800

    Update grafana from 7.5 to 8.1

--- modules/service/monitoring/grafana.nix
+++ modules/service/monitoring/grafana.nix
@@ -2,7 +2,7 @@
   config = {
     services.grafana = {
       enable = true;
-      package = grafana-latest;
+      package = pkgs.grafana-latest;
       domain = "localhost";
       port = 2342;
       addr = "127.0.0.1";
