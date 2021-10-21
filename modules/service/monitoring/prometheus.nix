67f518f264de8a656ea86e498f76f785f6e477d3
liuhh666233 <liuxiaobo666233@gmail.com>
2021-10-21 09:37:59 +0800

    Fix bug.

--- modules/service/monitoring/prometheus.nix
+++ modules/service/monitoring/prometheus.nix
@@ -25,10 +25,10 @@
                 queries = {
                   queryNetIncomeParent = {
                     help = "Examine exceptional wind FinancialStatement data.";
-                    labels = [ ];
+                    labels = [ "Date" "StockId" ];
                     query =
-                      "SELECT toDate(fs.ReportDate) as `Date`,fs.StockId,fs.NetIncomeParent from wind.FinancialStatement fs where fs.NetIncomeParent > 100000000000 order by fs.NetIncomeParent desc;";
-                    values = [ "Date" "StockId" "NetIncomeParent" ];
+                      "SELECT toString(toDate(fs.ReportDate)) as `Date`,toString(fs.StockId),fs.NetIncomeParent from wind.FinancialStatement fs where fs.NetIncomeParent > 100000000000 order by fs.NetIncomeParent desc;";
+                    values = [ "NetIncomeParent" ];
                   };
                 };
               };
