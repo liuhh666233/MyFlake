a75c1e5934fbfac48a782678badd79ff191fb625
liuhh666233 <liuxiaobo666233@gmail.com>
2021-11-08 16:48:58 +0800

    Add mysql package

---
+++ modules/service/mysql/default.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, ... }: {
+  services.mysql = {
+    enable = true;
+    port = 3306;
+    user = "mysql";
+    group = "mysql";
+    dataDir = "/var/lib/wonder/warehouse/mysql";
+    settings = literalExample ''
+      {
+        mysqld = {
+          key_buffer_size = "6G";
+          table_cache = 1600;
+          log-error = "/var/log/mysql_err.log";
+          plugin-load-add = [ "server_audit" "ed25519=auth_ed25519" ];
+        };
+        mysqldump = {
+          quick = true;
+          max_allowed_packet = "16M";
+        };
+      }
+    '';
+  };
+}
