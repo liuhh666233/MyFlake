4a939da71d78ca0e26daa41f80eddcdf3d3a5400
liuhh666233 <liuxiaobo666233@gmail.com>
2021-11-08 16:53:00 +0800

    Fixed bug.

--- modules/service/mysql/default.nix
+++ modules/service/mysql/default.nix
@@ -1,23 +1,23 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+with lib; {
   services.mysql = {
     enable = true;
     port = 3306;
+    package = pkgs.mariadb;
     user = "mysql";
     group = "mysql";
     dataDir = "/var/lib/wonder/warehouse/mysql";
-    settings = literalExample ''
-      {
-        mysqld = {
-          key_buffer_size = "6G";
-          table_cache = 1600;
-          log-error = "/var/log/mysql_err.log";
-          plugin-load-add = [ "server_audit" "ed25519=auth_ed25519" ];
-        };
-        mysqldump = {
-          quick = true;
-          max_allowed_packet = "16M";
-        };
-      }
-    '';
+    settings = {
+      mysqld = {
+        key_buffer_size = "6G";
+        table_cache = 1600;
+        log-error = "/var/log/mysql_err.log";
+        plugin-load-add = [ "server_audit" "ed25519=auth_ed25519" ];
+      };
+      mysqldump = {
+        quick = true;
+        max_allowed_packet = "16M";
+      };
+    };
   };
 }
