95150433b00ba51ecef381f597d20686357a032e
liuhh666233 <liuxiaobo666233@gmail.com>
2021-11-17 17:30:21 +0800

    Add ibus

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -4,11 +4,13 @@
   i18n = {
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
-    inputMethod = {
-      enabled = "fcitx5";
-      fcitx5.addons =
-        [ pkgs.fcitx5-rime-latest pkgs.fcitx5-chinese-addons-latest ];
-    };
+    # inputMethod = {
+    #   enabled = "fcitx5";
+    #   fcitx5.addons =
+    #     [ pkgs.fcitx5-rime-latest pkgs.fcitx5-chinese-addons-latest ];
+    # };
+    inputMethod.enabled = "ibus";
+    inputMethod.ibus.engines = pkgs.ibus-engines;
   };
 
   time.timeZone = "Asia/Shanghai";
