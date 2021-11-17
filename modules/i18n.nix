295d0526016da6c09bae56477107dc90206243b1
liuhh666233 <liuxiaobo666233@gmail.com>
2021-11-17 11:29:15 +0800

    Update uim

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -5,8 +5,8 @@
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
     inputMethod = {
-      enabled = "uim";
-      #   fcitx5.addons = [ pkgs.fcitx5-rime ];
+      enabled = "fcitx5";
+      fcitx5.addons = [ pkgs.fcitx5-rime pkgs.fcitx5-chinese-addons ];
     };
   };
 
