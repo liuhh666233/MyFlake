7c5569a26a45ef25335642386f9e95b9328fbdb3
liuhh666233 <liuxiaobo666233@gmail.com>
2021-11-17 09:50:35 +0800

    Update fctix5

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -6,7 +6,7 @@
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
     inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = [ pkgs.fcitx5-chinese-addons pkgs.fcitx5-rime ];
+      fcitx5.addons = [ pkgs.fcitx5-rime pkgs.fcitx5-chinese-addons ];
     };
   };
 
