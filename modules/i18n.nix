683b3567787da6d78eefe417570cbb0ab4db2414
lxb <liuxiaobo666233@gmail.com>
2021-11-22 10:16:51 +0800

    enable inputMethod fcitx5

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -4,10 +4,10 @@
   i18n = {
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
-    #  inputMethod = {
-    #    enabled = "fcitx5";
-    #    fcitx5.addons = [ pkgs.fcitx5-chinese-addons ];
-    #  };
+    inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = [ pkgs.fcitx5-chinese-addons ];
+    };
   };
 
   time.timeZone = "Asia/Shanghai";
