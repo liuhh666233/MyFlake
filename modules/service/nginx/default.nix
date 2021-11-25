a5b957ded097504d9ee7e11af21f9061162f2442
lxb <liuxiaobo666233@gmail.com>
2021-11-25 13:12:39 +0800

    Update nginx

--- modules/service/nginx/default.nix
+++ modules/service/nginx/default.nix
@@ -27,36 +27,52 @@ in {
       recommendedTlsSettings = true;
       # Enable recommended optimisation settings.
       recommendedOptimisation = true;
+      config = ''
+        server {
+          listen       10000;
+          server_name  localhost;
+          location / {
+            proxy_pass http://127.0.0.1:5555;
+            proxy_set_header Host $host:10000;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
 
+            proxy_http_version    1.1;
+            proxy_set_header      Upgrade "websocket";
+            proxy_set_header      Connection "Upgrade";
+            proxy_read_timeout    86400;
+          }
+        }
+      '';
       # nginx 代理jupyter notebook  
-      virtualHosts."127.0.0.1" = proxyConfig {
-        listenPort = 10000;
-        extraConfig = ''
-          proxy_pass http://127.0.0.1:5555;
-          proxy_set_header Host $host:10000;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      # virtualHosts."127.0.0.1" =   {
+      #   listenPort = 10000;
+      #   extraConfig = ''
+      #     proxy_pass http://127.0.0.1:5555;
+      #     proxy_set_header Host $host:10000;
+      #     proxy_set_header X-Real-IP $remote_addr;
+      #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
 
-          proxy_http_version    1.1;
-          proxy_set_header      Upgrade "websocket";
-          proxy_set_header      Connection "Upgrade";
-          proxy_read_timeout    86400;
-        '';
-      };
+      #     proxy_http_version    1.1;
+      #     proxy_set_header      Upgrade "websocket";
+      #     proxy_set_header      Connection "Upgrade";
+      #     proxy_read_timeout    86400;
+      #   '';
+      # };
 
-      virtualHosts."192.168.0.110" = {
-        forceSSL = false;
-        enableACME = false;
-        listen = [{
-          addr = "0.0.0.0";
-          port = 10001;
-        }];
-        locations."/" = {
-          proxyPass =
-            "http://127.0.0.1:${toString config.services.grafana.port}";
-          proxyWebsockets = true;
-        };
-      };
+      # virtualHosts."192.168.0.110" = {
+      #   forceSSL = false;
+      #   enableACME = false;
+      #   listen = [{
+      #     addr = "0.0.0.0";
+      #     port = 10001;
+      #   }];
+      #   locations."/" = {
+      #     proxyPass =
+      #       "http://127.0.0.1:${toString config.services.grafana.port}";
+      #     proxyWebsockets = true;
+      #   };
+      # };
     };
 
     networking.firewall.allowedTCPPorts = [ 10000 ];
