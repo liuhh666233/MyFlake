0cd46a452050fd212a8462217ac36c612db367c8
lxb <liuxiaobo666233@gmail.com>
2021-12-03 16:02:49 +0800

    Add inputMethod

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -4,10 +4,6 @@
   i18n = {
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
-    inputMethod = {
-      enabled = "fcitx5";
-      fcitx5.addons = [ pkgs.fcitx5-chinese-addons ];
-    };
   };
 
   time.timeZone = "Asia/Shanghai";
@@ -15,5 +11,17 @@
   location = {
     latitude = 23.0;
     longitude = 113.0;
+  };
+
+  systemd.services.inputMethod = {
+    description = "Start inputMethod.";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart =
+        "/nix/store/wv35g5lff84rray15zlzarcqi9fxzz84-bash-4.4-p23/bin/bash -e /nix/store/vng7i5ilp17m6j41xy4nxjzrpb8fp3zh-unit-script-fcitx5-daemon-start/bin/fcitx5-daemon-start";
+      KillMode = "process";
+      Restart = "on-failure";
+    };
   };
 }
