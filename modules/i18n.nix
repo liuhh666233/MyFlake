bab6168caee6f2c5cacc63571cafcf8ce5c706b3
lxb <liuxiaobo666233@gmail.com>
2021-12-03 16:08:01 +0800

    delete inputMethod

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -13,15 +13,4 @@
     longitude = 113.0;
   };
 
-  systemd.services.inputMethod = {
-    description = "Start inputMethod.";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart =
-        "/nix/store/wv35g5lff84rray15zlzarcqi9fxzz84-bash-4.4-p23/bin/bash -e /nix/store/vng7i5ilp17m6j41xy4nxjzrpb8fp3zh-unit-script-fcitx5-daemon-start/bin/fcitx5-daemon-start";
-      KillMode = "process";
-      Restart = "on-failure";
-    };
-  };
 }
