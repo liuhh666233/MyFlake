1d436790ec4c098a50d98e7083893d95094c971a
lxb <liuxiaobo666233@gmail.com>
2022-03-18 09:35:02 +0800

    卸载gnome中无需使用的package

--- machines/lxb/default.nix
+++ machines/lxb/default.nix
@@ -29,6 +29,37 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.gnome.chrome-gnome-shell.enable = true;
   services.gnome.gnome-remote-desktop.enable = true;
+  # Disable gnome all apps
+  # services.gnome.core-utilities.enable = false;
+
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    baobab # disk usage analyzer
+    cheese # photo booth
+    eog # image viewer
+    epiphany # web browser
+    simple-scan # document scanner
+    totem # video player
+    yelp # help viewer
+    evince # document viewer
+    file-roller # archive manager
+    seahorse # password manager
+
+    # these should be self explanatory
+    gnome-characters
+    gnome-clocks
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-photos
+    gnome-screenshot
+    gnome-system-monitor
+    gnome-weather
+    gnome-disk-utility
+    pkgs.gnome-connections
+  ];
+
   system.stateVersion = "21.05";
 
   virtualisation.docker.enable = true;
