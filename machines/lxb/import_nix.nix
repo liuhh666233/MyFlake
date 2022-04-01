efd439c514a06119014a73933343faec34ae2889
lxb <liuxiaobo666233@gmail.com>
2022-04-01 15:44:05 +0800

    Add wonder binary chaches and remote nix build.

--- machines/lxb/import_nix.nix
+++ machines/lxb/import_nix.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 let
-
+  baseParams = config.wonder.baseParams;
+  vitalParams = config.wonder.vitalParams;
 in {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -9,4 +10,19 @@ in {
     ../../modules/service/jupyter
     ../../users/lxb.nix
   ];
+
+  wonder.binaryCaches = {
+    enable = true;
+    nixServe.host = baseParams.hosts.sisyphus;
+    nixServe.port = baseParams.ports.nixServerPort;
+  };
+
+  wonder.remoteNixBuild = {
+    enable = true;
+    user = "root";
+    host = baseParams.hosts.sisyphus;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
 }
