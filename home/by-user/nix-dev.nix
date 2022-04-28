3817a9badf2302c0b1fb6f2a628edb18b10d119b
lxb <liuxiaobo666233@gmail.com>
2022-04-28 10:07:08 +0800

    Update programs fish.

--- home/by-user/nix-dev.nix
+++ home/by-user/nix-dev.nix
@@ -8,28 +8,28 @@
   #   programs.zsh.enable = true;
   # Or any other shell you're using.
 
-  programs.fish.enable = true;
-
-  programs.fish.shellAliases = {
-    "gs" = "git status";
-    "gsw" = "git switch";
-    "ga" = "git add";
-    "gl" = "git log";
-    "gp" = "git push";
-    "gpp" = "git pull";
-    "gc" = "git commit -m";
-    "gb" = "git branch";
-    "gd" = "git diff";
-    "grep" = "rg";
-    "cat" = "bat";
-    "cc" = "code -n";
-    "gg" = "gedit";
-    "pr" = "poetry run";
-    "pt" = "poetry run pytest -v";
-    "gla" = "glances";
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      "gs" = "git status";
+      "gsw" = "git switch";
+      "ga" = "git add";
+      "gl" = "git log";
+      "gp" = "git push";
+      "gpp" = "git pull";
+      "gc" = "git commit -m";
+      "gb" = "git branch";
+      "gd" = "git diff";
+      "grep" = "rg";
+      "cat" = "bat";
+      "cc" = "code -n";
+      "gg" = "gedit";
+      "pr" = "poetry run";
+      "pt" = "poetry run pytest -v";
+      "gla" = "glances";
+    };
+    shellInit = ''
+      source (${pkgs.z-lua}/bin/z --init fish | psub)
+    '';
   };
-
-  programs.fish.shellInit = ''
-    source (${pkgs.z-lua}/bin/z --init fish | psub)
-  '';
 }
