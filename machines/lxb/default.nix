54a372e2f4b000bc8be3dd2b4560fecae5a931bd
lxb <liuxiaobo666233@gmail.com>
2022-04-29 18:21:08 +0800

    Fixed machince/

--- machines/lxb/default.nix
+++ machines/lxb/default.nix
@@ -11,7 +11,7 @@ in {
     ../../users/lxb.nix
   ];
 
-  vital.mainUser = "liuxiaobo";
+  vital.mainUser = "lxb";
 
   vital.graphical.enable = true;
 
--- users/lxb.nix
+++ users/lxb.nix
@@ -3,12 +3,7 @@
   users = {
     users.lxb = {
       isNormalUser = true;
-      uid = 1001;
-      initialHashedPassword = lib.mkDefault
-        "$6$c7xWgHnMCJKXlPHK$2Owbgw3Y5z/pgwD76O45nPzuRtABjN.Mr6M.yO1jOteDyVsXSOqgzgkAl1sD1kIowEQRFIoyXT45j8ZLmZ5SF1";
-      description = "The user lxb.";
       home = "/home/lxb";
-      extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
       shell = pkgs.fish;
     };
   };
