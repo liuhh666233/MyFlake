522f48be59045b2c81b4420d88d35e6fb00bd9e6
lxb <liuxiaobo666233@gmail.com>
2022-08-01 17:41:38 +0800

    Update jupyter

--- modules/service/jupyter/default.nix
+++ modules/service/jupyter/default.nix
@@ -3,8 +3,8 @@
     enable = true;
     ip = "localhost";
     port = 5555;
-    notebookDir = "/home/nixos/python";
-    user = "nixos";
+    notebookDir = "/home/${config.vital.mainUser}/python";
+    user = config.vital.mainUser;
     group = "users";
     password =
       "'argon2:$argon2id$v=19$m=10240,t=10,p=8$OEgjjTojGX8e+bmi431VBg$WjUsHTZdM2EOIjpwgn5dXw'";
