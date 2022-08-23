e357230d09f5d545775125b8cd05880d9fff9faf
lxb <liuxiaobo6663233@gmail.com>
2022-08-24 07:41:09 +0800

    Update powerManagement.cpuFreqGovernor to powersave in nas

--- machines/nas/hardware-configuration.nix
+++ machines/nas/hardware-configuration.nix
@@ -4,34 +4,32 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8385410b-127b-41fa-a0eb-266efd7e76c8";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/8385410b-127b-41fa-a0eb-266efd7e76c8";
+    fsType = "ext4";
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/3fb861b1-b606-4137-9639-3b7dc947aee7"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/3fb861b1-b606-4137-9639-3b7dc947aee7"; }];
 
-  fileSystems."/var/lib/wonder/warehouse" =
-    { device = "/dev/disk/by-uuid/04e86efb-6c72-4cc4-be41-39ae2ab28a54";
-      fsType = "ext4";
-    };
+  fileSystems."/var/lib/wonder/warehouse" = {
+    device = "/dev/disk/by-uuid/04e86efb-6c72-4cc4-be41-39ae2ab28a54";
+    fsType = "ext4";
+  };
 
-  fileSystems."/var/lib/wonder/nas" =
-    { device = "/dev/disk/by-uuid/cb37c05d-be98-48f6-a628-888a819054a8";
-      fsType = "ext4";
-    };
-    
+  fileSystems."/var/lib/wonder/nas" = {
+    device = "/dev/disk/by-uuid/cb37c05d-be98-48f6-a628-888a819054a8";
+    fsType = "ext4";
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
@@ -39,5 +37,8 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp30s0.useDHCP = lib.mkDefault true;
 
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
