ef616170fcaefb0f55fee188f851c142d5d7da41
lxb <liuxiaobo666233@gmail.com>
2022-09-30 14:57:16 +0800

    Add homeConfigurations.lxb.

--- flake.nix
+++ flake.nix
@@ -13,13 +13,19 @@
     wonder-foundations.url =
       "git+ssh://git@github.com/quant-wonderland/wonder-foundations?ref=master";
     wonder-foundations.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager?rev=93a69d07389311ffd6ce1f4d01836bbc2faec644";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, wonder-foundations
-    , ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, wonder-foundations, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        config.allowBroken = true;
+      };
+      pkgs-unstable = import nixpkgs-unstable {
         inherit system;
         config.allowUnfree = true;
         config.allowBroken = true;
@@ -71,6 +77,13 @@
           modules = [ ./machines/demo-vm ];
         };
 
+      };
+
+      homeConfigurations.lxb = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs system;
+        username = "lxb";
+        homeDirectory = "/home/lxb";
+        configuration = import ./home/lxb.nix;
       };
     };
 }
---
+++ home/lxb.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [ pkgs.fd ];
+
+  programs.home-manager.enable = true;
+
+  # ...other config, other config...
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  programs.fzf.enable = true;
+  programs.bat.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      "gs" = "git status";
+      "ga" = "git add";
+      "gl" = "git log";
+      "gp" = "git push";
+      "gc" = "git commit -m";
+      "gb" = "git branch";
+      "gd" = "git diff";
+      "cc" = "code -n";
+      "pr" = "poetry run";
+      "pt" = "poetry run pytest -v";
+    };
+    shellInit = ''
+      source (${pkgs.z-lua}/bin/z --init fish | psub)
+      set fzf_fd_opts --hidden --exclude=.git
+      fzf_configure_bindings --git_status=\cg --history=\ch --processes=\co --variables --directory --git_log
+      set --global hydro_color_pwd blue
+      set --global hydro_color_git green
+      set --global hydro_color_duration cyan
+    '';
+    plugins = [
+    {
+      name = "z";
+      src = pkgs.fetchFromGitHub {
+        owner = "jethrokuan";
+        repo = "z";
+        rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+        sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+      };
+    }
+    { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+    { name = "hydro"; src = pkgs.fishPlugins.hydro.src; }
+  ];
+  };
+}
