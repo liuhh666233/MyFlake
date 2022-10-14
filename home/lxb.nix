7bf423d6fdfa7b852f1c60be56e927abcbf6bd6b
lxb <liuxiaobo666233@gmail.com>
2022-10-14 20:43:00 +0800

    Update lxb.nix

--- home/lxb.nix
+++ home/lxb.nix
@@ -15,28 +15,23 @@
     enable = true;
     shellAliases = {
       "gs" = "git status";
-      # "ga" = "git add";
+      "ga" = "git add";
       "gl" = "git log";
       "gp" = "git push";
       "gc" = "git commit -m";
       "gb" = "git branch";
-      # "gd" = "git diff";
+      "gd" = "git diff";
       "cc" = "code -n";
       "pr" = "poetry run";
       "pt" = "poetry run pytest -v";
     };
 
-    interactiveShellInit = ''
-      printf '%s@%s%s %s%s %s' $USER (set_color red) $hostname (set_color normal) (date '+%y/%m/%d') \n
-    '';
-
     shellInit = ''
       source (${pkgs.z-lua}/bin/z --init fish | psub)
+
       set fzf_fd_opts --hidden --exclude=.git
+
       fzf_configure_bindings --git_status=\cg --history=\ch --processes=\co --variables --directory --git_log
-      set --global hydro_color_pwd blue
-      set --global hydro_color_git green
-      set --global hydro_color_duration cyan
     '';
     plugins = [
     {
@@ -48,9 +43,19 @@
         sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
       };
     }
-    { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
-    { name = "hydro"; src = pkgs.fishPlugins.hydro.src; }
-    { name = "forgit"; src = pkgs.fishPlugins.forgit.src; }
+    {
+      name = "fzf-fish";
+      src = pkgs.fishPlugins.fzf-fish.src;
+    }
+    {
+      name="foreign-env";
+      src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+      };
+    }
   ];
   };
 }
