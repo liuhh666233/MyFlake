230002c63940fb01fcc9548dd0c3297894b11b99
lxb <liuxiaobo666233@gmail.com>
2022-10-14 20:40:23 +0800

    Update nix-dev.nix.

--- home/by-user/nix-dev.nix
+++ home/by-user/nix-dev.nix
@@ -47,6 +47,15 @@
         name = "fzf-fish";
         src = pkgs.fishPlugins.fzf-fish.src;
       }
+      {
+        name="foreign-env";
+        src = pkgs.fetchFromGitHub {
+            owner = "oh-my-fish";
+            repo = "plugin-foreign-env";
+            rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+            sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+        };
+      }
     ];
   };
 }
