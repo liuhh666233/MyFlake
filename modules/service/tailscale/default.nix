783c0054ed6db881a0770800a2ff62bd76310597
lxb <liuxiaobo6663233@gmail.com>
2023-05-28 08:27:11 +0800

    Add add_tailscale_routes service.

--- modules/service/tailscale/default.nix
+++ modules/service/tailscale/default.nix
@@ -8,4 +8,16 @@
 
   environment.systemPackages = [ pkgs.tailscale ];
 
+  systemd.services.add_tailscale_routes = {
+    description = "Tailscale routes script";
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      # Contents of your startup script
+      # Replace this with your actual script
+      echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+      echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+      sudo sysctl -p /etc/sysctl.conf
+    '';
+  };
+
 }
