0495ac8cf9108b92623928da765f583078455834
lxb <liuxiaobo6663233@gmail.com>
2023-06-17 07:02:35 +0800

    Fix add_tailscale_routes.

--- modules/service/tailscale/default.nix
+++ modules/service/tailscale/default.nix
@@ -14,10 +14,11 @@
     script = ''
       # Contents of your startup script
       # Replace this with your actual script
-      echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
-      echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
-      sudo sysctl -p /etc/sysctl.conf
+      echo 'net.ipv4.ip_forward = 1' | /run/wrappers/bin/sudo tee -a /etc/sysctl.conf
+      echo 'net.ipv6.conf.all.forwarding = 1' | /run/wrappers/bin/sudo tee -a /etc/sysctl.conf
+      /run/wrappers/bin/sudo sysctl -p /etc/sysctl.conf
     '';
+    path = [pkgs.procps];
   };
 
 }
