bbe7cf0c7f8e2e8b00209637c9e39fdc6518a1d8
lxb <liuxiaobo666233@gmail.com>
2025-10-02 17:11:48 +0800

    Update Jupyter service configuration to allow remote access and change IP binding

--- modules/service/jupyter/default.nix
+++ modules/service/jupyter/default.nix
@@ -1,21 +1,19 @@
 { config, pkgs, ... }: {
   services.jupyter = {
     enable = true;
-    ip = "localhost";
+    ip = "0.0.0.0";
     port = 5555;
     notebookDir = "/home/${config.vital.mainUser}/python";
     user = config.vital.mainUser;
     group = "users";
     password =
       "'argon2:$argon2id$v=19$m=10240,t=10,p=8$OEgjjTojGX8e+bmi431VBg$WjUsHTZdM2EOIjpwgn5dXw'";
-    notebookConfig = "c.NotebookApp.allow_remote_access = True";
     kernels = {
       python3 = let
         env = (pkgs.python3.withPackages (pythonPackages:
           with pythonPackages; [
             jupyter
             jupyter_core
-            jupyterlab
             notebook
             ipython
             ipykernel
